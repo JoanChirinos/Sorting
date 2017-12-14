6d2493c664003d87f5c3208b8bf32007037465b2
JoanChirinos <jchirinos@stuy.edu>
2017-12-13 21:30:17 -0500

    Copied comments from MySorts to SortTester. It's so good xD

--- MySorts.java
+++ MySorts.java
@@ -3,7 +3,7 @@
 // HW53 --
 // 2017-12-14
 
-
+import java.util.ArrayList;
 
 public class MySorts {
     
@@ -11,18 +11,22 @@ public class MySorts {
     // postcondition: data's elements sorted in ascending order
     public static void bubbleSort( ArrayList<Comparable> data )
     {
+	int swapcount = 0, compcount = 0;
 	//make n-1 passes across collection
 	for( int passCtr = 1; passCtr < data.size(); passCtr++ ) {
-	    System.out.println( "commencing pass #" + passCtr + "..." );
 
 	    //iterate from first to next-to-last element
 	    for( int i = 0; i < data.size()-1; i++ ) {
 		//if element at i > element at i+1, swap
-		if ( data.get(i).compareTo(data.get(i+1) ) > 0 ) 
+		compcount++;
+		if ( data.get(i).compareTo(data.get(i+1) ) > 0 )
+		    swapcount++;
 		    data.set( i, data.set(i+1,data.get(i)) );	
 		//System.out.println(data); //diag: show current state of list
 	    }
 	}
+	System.out.println("# of comparisons: " + compcount);
+	System.out.println("# of swaps: " + swapcount);
     }
 
     /**********
@@ -42,23 +46,24 @@ public class MySorts {
     // postcondition: data's elements sorted in ascending order
     public static void selectionSort( ArrayList<Comparable> data ) 
     {
+	int swapcount = 0, compcount = 0;
 	//note: this version places greatest value at rightmost end,
 
 	//maxPos will point to position of SELECTION (greatest value)
 	int maxPos;
 
 	for( int pass = data.size()-1; pass > 0; pass-- ) {
-	    System.out.println( "\nbegin pass " + (data.size()-pass) );//diag
 	    maxPos = 0;
 	    for( int i = 1; i <= pass; i++ ) {
-		System.out.println( "maxPos: " + maxPos );//diag
-		System.out.println( data );//diag
+		compcount++;
 		if ( data.get(i).compareTo( data.get(maxPos) ) > 0 )
 		    maxPos = i;
 	    }
+	    swapcount++;
 	    data.set( maxPos, ( data.set( pass, data.get(maxPos) ) ) );
-	    System.out.println( "after swap: " +  data );//diag
 	}
+	System.out.println("# of comparisons: " + compcount);
+	System.out.println("# of swaps: " + swapcount);
     }//end selectionSort
 
     /**********
@@ -71,27 +76,26 @@ public class MySorts {
     // postcondition: data's elements sorted in ascending order
     public static void insertionSort( ArrayList<Comparable> data )
     {
+	int swapcount = 0, compcount = 0;
 	for( int partition = 1; partition < data.size(); partition++ ) {
 	    //partition marks first item in unsorted region
-
-	    //diag:
-	    System.out.println( "\npartition: " + partition + "\tdataset:");
-	    System.out.println( data ); 
 
 	    //traverse sorted region from right to left
 	    for( int i = partition; i > 0; i-- ) {
 
 		// "walk" the current item to where it belongs
 		// by swapping adjacent items
+		compcount++;
 		if ( data.get(i).compareTo( data.get(i-1) ) < 0 ) {
-		    //diag:
-		    System.out.println( "swap indices "+(i-1)+" & "+i+"..." );
+		    swapcount++;
 		    data.set( i, data.set( i-1, data.get(i) ) ); 
 		}
 		else 
 		    break; 
 	    }
 	}
+	System.out.println("# of comparisons: " + compcount);
+	System.out.println("# of swaps: " + swapcount);
     }//end insertionSort
 
     /**********
--- SortTester.java
+++ SortTester.java
@@ -5,7 +5,41 @@
   2017-12-14
 */
 
+    /**********
+     * Best Case for bubbleSort: An AL in ascending order
+     *    Why? Because the way we have it written, it has a constant number
+     *    of comparisons (if AL size is constant). However, it only swaps if
+     *    the element is in the wrong place. It would be better to tell the
+     *    method to stop running once it passes without any swaps
+     * Worst Case for bubbleSort: An AL in descending order
+     *    Why? As previously stated, bubbleSort will do the same number of
+     *    comparisons no matter the order. However, it will do the greatest
+     *    number of swaps when the AL is in descending order. (I think it's
+     *    about 0.5n^2 - 0.5n swaps . I did some trials and graphing stuff)
+     **********/
+
+    /**********
+     * There is no worst or best case scenario for selectionSort  because, no
+     * matter what, selectionSort has to do a constant number of comparisons
+     * and swaps (even if an element swaps with itself)
+     **********/
+
+    /**********
+     * Best case scenario for insertionSort: AL sorted in ascending order
+     *    Why? Because like this, no elements will swap places. This will also
+     *    result in the least number of comparisons
+     * Worst case scenario for insertionSort: AL sorted in descending order
+     *    Why? Because this way, it works sort of like a type of bubble(ish)
+     *    sorting. Starting at element 1 and working towards the last element,
+     *    each element will swap with the adjacent element with a lower index
+     *    until it reaches index 0. This will result in the greatest number of
+     *    swaps and comparisons.
+     **********/
+
+import java.util.ArrayList;
+
 public class SortTester {
+
 
     public static ArrayList populate( int size, int lo, int hi ) {
 	ArrayList<Integer> retAL = new ArrayList<Integer>();
@@ -28,7 +62,72 @@ public class SortTester {
     }
     
     public static void main(String[] args) {
-	
-    }
 
+	/***************************
+	 * Creating+Populating ALs *
+	 ***************************/
+	
+	ArrayList<Comparable> bestbubble = new ArrayList<Comparable>();
+	for (int i = 1; i < 6; i++)
+	    bestbubble.add(i);
+	
+	ArrayList<Comparable> worstbubble = new ArrayList<Comparable>();
+	for (int i = 5; i > 0; i--)
+	    worstbubble.add(i);
+
+	
+	ArrayList<Comparable> ascendingselect = new ArrayList<Comparable>();
+	for (int i = 1; i < 6; i++)
+	    ascendingselect.add(i);
+	
+	ArrayList<Comparable> descendingselect = new ArrayList<Comparable>();
+	for (int i = 5; i > 0; i--)
+	    descendingselect.add(i);
+
+	ArrayList<Comparable> randomselect = new ArrayList<Comparable>();
+	for (int i = 0; i < 5; i++)
+	    randomselect.add(i);
+	shuffle(randomselect);
+	
+	ArrayList<Comparable> bestinsert = new ArrayList<Comparable>();
+	for (int i = 1; i < 6; i++)
+	    bestinsert.add(i);
+	
+	ArrayList<Comparable> worstinsert = new ArrayList<Comparable>();
+	for (int i = 5; i > 0; i--)
+	    worstinsert.add(i);
+
+	/**********************
+	 * Sorting goes below *
+	 **********************/
+
+	System.out.println("bestbubble: ");
+	MySorts.bubbleSort(bestbubble);
+	System.out.println();
+
+	System.out.println("worstbubble: ");
+	MySorts.bubbleSort(worstbubble);
+	System.out.println();
+
+	System.out.println("ascendingselect: ");
+	MySorts.selectionSort(ascendingselect);
+	System.out.println();
+
+	System.out.println("descendingselect: ");
+	MySorts.selectionSort(descendingselect);
+	System.out.println();
+
+	System.out.println("randomselect: ");
+	MySorts.selectionSort(randomselect);
+	System.out.println();
+
+	System.out.println("bestinsert: ");
+	MySorts.insertionSort(bestinsert);
+	System.out.println();
+
+	System.out.println("worstinsert: ");
+	MySorts.insertionSort(worstinsert);
+	System.out.println();
+    }
+    
 }
