acc310617e8366a93d50fe835aeebbc5d3317827
JoanChirinos <jchirinos@stuy.edu>
2017-12-13 21:04:10 -0500

    Added comments and stuff to MySorts

--- MySorts.java
+++ MySorts.java
@@ -7,10 +7,9 @@
 
 public class MySorts {
     
-    // VOID version of bubbleSort
     // Rearranges elements of input ArrayList
     // postcondition: data's elements sorted in ascending order
-    public static void bubbleSortV( ArrayList<Comparable> data )
+    public static void bubbleSort( ArrayList<Comparable> data )
     {
 	//make n-1 passes across collection
 	for( int passCtr = 1; passCtr < data.size(); passCtr++ ) {
@@ -25,11 +24,23 @@ public class MySorts {
 	    }
 	}
     }
+
+    /**********
+     * Best Case for bubbleSort: An AL in ascending order
+     *    Why? Because the way we have it written, it has a constant number
+     *    of comparisons (if AL size is constant). However, it only swaps if
+     *    the element is in the wrong place. It would be better to tell the
+     *    method to stop running once it passes without any swaps
+     * Worst Case for bubbleSort: An AL in descending order
+     *    Why? As previously stated, bubbleSort will do the same number of
+     *    comparisons no matter the order. However, it will do the greatest
+     *    number of swaps when the AL is in descending order. (I think it's
+     *    about 0.5n^2 - 0.5n swaps . I did some trials and graphing stuff)
+     **********/
     
-    // VOID version of SelectionSort
     // Rearranges elements of input ArrayList
     // postcondition: data's elements sorted in ascending order
-    public static void selectionSortV( ArrayList<Comparable> data ) 
+    public static void selectionSort( ArrayList<Comparable> data ) 
     {
 	//note: this version places greatest value at rightmost end,
 
@@ -48,12 +59,17 @@ public class MySorts {
 	    data.set( maxPos, ( data.set( pass, data.get(maxPos) ) ) );
 	    System.out.println( "after swap: " +  data );//diag
 	}
-    }//end selectionSortV
+    }//end selectionSort
 
-    // VOID version of InsertionSort
+    /**********
+     * There is no worst or best case scenario for selectionSort  because, no
+     * matter what, selectionSort has to do a constant number of comparisons
+     * and swaps (even if an element swaps with itself)
+     **********/
+
     // Rearranges elements of input ArrayList
     // postcondition: data's elements sorted in ascending order
-    public static void insertionSortV( ArrayList<Comparable> data )
+    public static void insertionSort( ArrayList<Comparable> data )
     {
 	for( int partition = 1; partition < data.size(); partition++ ) {
 	    //partition marks first item in unsorted region
@@ -76,5 +92,17 @@ public class MySorts {
 		    break; 
 	    }
 	}
-    }//end insertionSortV
+    }//end insertionSort
+
+    /**********
+     * Best case scenario for insertionSort: AL sorted in ascending order
+     *    Why? Because like this, no elements will swap places. This will also
+     *    result in the least number of comparisons
+     * Worst case scenario for insertionSort: AL sorted in descending order
+     *    Why? Because this way, it works sort of like a type of bubble(ish)
+     *    sorting. Starting at element 1 and working towards the last element,
+     *    each element will swap with the adjacent element with a lower index
+     *    until it reaches index 0. This will result in the greatest number of
+     *    swaps and comparisons.
+     **********/
 }
